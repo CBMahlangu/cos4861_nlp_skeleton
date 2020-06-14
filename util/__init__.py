11442e44608bc7d44b0ef68b9f779388732ab122
xcfm <b2801q@yahoo.com>
2020-06-14 20:54:42 +0200

    code for distance method previously tested remotely

--- util/__init__.py
+++ util/__init__.py
@@ -41,11 +41,39 @@ class DistanceCalculator:
         self._deletion_cost = deletion_cost
         self._subst_cost = subst_cost
 
-    def distance(self, source, target):
-        """
-        Calculates the distance between two strings.
-        :param source: The source string
-        :param target: The target string
-        :return: The scalar distance between the source and target.
-        """
-        raise NotImplementedError('Distance calculation not implemented yet')
+    def distance(source, target):
+
+	    #length of each word
+	    l_target = len(target) #length of target
+	    l_source = len(source) #length of source
+
+	    #add one unit to length of word(s)
+	    lT = l_target + 1 # target length plus one
+	    lS = l_source + 1 # source length plus one
+
+	    #create a matrix
+	    matrix = [[0 for y in range(lT)] for x in range(lS)] 
+
+	    #Initialize column
+	    for x in range(lS):
+		    matrix[x][0] = x
+
+	    #Initialize row
+	    for y in range(lT):
+		    matrix[0][y] = y
+
+	    for x in range(1,lS):
+		    for y in range(1,lT):
+
+			    if target[y-1] == source[x-1]: # provided letters are equal
+				    matrix[x][y] = matrix[x-1][y-1]
+			    else:							# when letters are not equal, add one to distance
+				    matrix[x][y] = min([matrix[x-1][y],matrix[x][y-1],matrix[x-1][y-1]]) + 1
+	    print(matrix)
+	    print("")
+	    return matrix[l_source][l_target]
+
+    sWord = input("Enter source word : ")
+    tWord = input("Enter target word : ")
+    res = distance(tWord,sWord)
+    print(res)
